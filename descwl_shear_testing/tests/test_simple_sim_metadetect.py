d239d60773358754e590524fc70996a588e3642d
Erin Sheldon <erin.sheldon@gmail.com>
2019-12-20 12:40:24 -0500

    added smoke test for metadetect

---
+++ descwl_shear_testing/tests/test_simple_sim_metadetect.py
@@ -0,0 +1,44 @@
+import numpy as np
+# import pytest
+import ngmix
+
+from ..simple_sim import Sim
+from ..coadd_obs import CoaddObs
+from ..metadetect import SimMetadetect
+
+
+def test_simple_sim_metadetect_smoke():
+    rng = np.random.RandomState(9123)
+    sim = Sim(rng=rng)
+    data = sim.gen_sim()
+
+    # faking ngmix MultiBandObsList
+    # note data is an OrderedDict
+    coadd_mbobs = ngmix.MultiBandObsList(
+        meta={'psf_fwhm': sim.psf_kws['fwhm']},
+    )
+    for band in data:
+        coadd_obs = CoaddObs(data[band])
+        obslist = ngmix.ObsList()
+        obslist.append(coadd_obs)
+        coadd_mbobs.append(obslist)
+
+    config = {
+        'bmask_flags': 0,
+        'metacal': {
+            'use_noise_image': True,
+            'psf': 'fitgauss',
+        },
+        'psf': {
+            'model': 'gauss',
+            'lm_pars': {},
+            'ntry': 2,
+        },
+        'weight': {
+            'fwhm': 1.2,
+        },
+        'meds': {},
+    }
+
+    md = SimMetadetect(config, coadd_mbobs, rng)
+    md.go()
