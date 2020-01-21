bad86d951be593629a8e12e231235aa60eaad0cf
Erin Sheldon <erin.sheldon@gmail.com>
2020-01-21 11:53:54 -0500

    add EDGE test

--- descwl_shear_sims/tests/test_lsst_mask_bits.py
+++ descwl_shear_sims/tests/test_lsst_mask_bits.py
@@ -6,16 +6,20 @@ try:
 except ImportError:
     HAVE_STACK = False
 
-from ..simple_sim import COSMIC_RAY, BAD_COLUMN
+from ..simple_sim import (
+    COSMIC_RAY,
+    BAD_COLUMN,
+    EDGE,
+)
 
 
 @pytest.mark.skipif(not HAVE_STACK, reason='the DM stack is not installed')
 def test_lsst_mask_bits():
-    mask = afw_image.Mask()
-    cr_val = 2**mask.getMaskPlane('CR')
+    cr_val = 2**afw_image.Mask.getMaskPlane('CR')
     assert cr_val == COSMIC_RAY
 
-    mask = afw_image.Mask()
-    bad_val = 2**mask.getMaskPlane('BAD')
-
+    bad_val = 2**afw_image.Mask.getMaskPlane('BAD')
     assert bad_val == BAD_COLUMN
+
+    edge_val = 2**afw_image.Mask.getMaskPlane('EDGE')
+    assert edge_val == EDGE
