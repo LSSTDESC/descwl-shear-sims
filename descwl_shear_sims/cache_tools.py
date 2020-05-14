f8d2f95f355cbe140b56afa386584e2cb36e7ec9
Erin Sheldon <erin.sheldon@gmail.com>
2020-05-14 14:11:58 -0400

    use json file with list of example headers

--- descwl_shear_sims/cache_tools.py
+++ descwl_shear_sims/cache_tools.py
@@ -5,8 +5,3 @@ import fitsio
 @functools.lru_cache(maxsize=8)
 def cached_catalog_read(fname):
     return fitsio.read(fname)
-
-
-@functools.lru_cache(maxsize=8)
-def cached_header_read(*, fname, ext):
-    return fitsio.read_header(fname, ext=ext)
