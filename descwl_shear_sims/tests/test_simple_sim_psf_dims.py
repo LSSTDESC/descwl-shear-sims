747474a38b7ac91a15a59771f85efe25b9dbd0e8
Erin Sheldon <erin.sheldon@gmail.com>
2020-05-26 10:27:10 -0400

    test of setting psf dims

---
+++ descwl_shear_sims/tests/test_simple_sim_psf_dims.py
@@ -0,0 +1,19 @@
+from ..simple_sim import Sim
+
+
+def test_simple_sim_psf_dims():
+    psf_dim = 101
+    sim = Sim(
+        rng=10,
+        psf_dim=psf_dim,
+        gals_kws={'density': 10},
+    )
+
+    data = sim.gen_sim()
+
+    for band in sim.bands:
+        assert len(data[band]) == sim.epochs_per_band
+        for epoch in range(sim.epochs_per_band):
+            epoch_obs = data[band][epoch]
+            psf_im = epoch_obs.get_psf(10, 3)
+            assert psf_im.array.shape == (psf_dim, psf_dim)
