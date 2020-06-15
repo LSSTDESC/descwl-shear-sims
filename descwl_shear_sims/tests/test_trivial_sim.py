fb3d8b773e3c4be0a5114fe6c3106d833a0fe7e3
Erin Sheldon <erin.sheldon@gmail.com>
2020-06-15 15:40:36 -0400

    tests for refactor

--- descwl_shear_sims/tests/test_trivial_sim.py
+++ descwl_shear_sims/tests/test_trivial_sim.py
@@ -1,6 +1,9 @@
 import pytest
 import numpy as np
-from ..trivial_sim import make_trivial_sim
+from ..trivial_sim import (
+    make_trivial_sim,
+    make_galaxy_catalog,
+)
 
 
 @pytest.mark.parametrize('dither,rotate', [
@@ -13,12 +16,20 @@ def test_trivial_sim_smoke(dither, rotate):
 
     seed = 74321
     rng = np.random.RandomState(seed)
+
+    coadd_dim = 341
+    galaxy_catalog = make_galaxy_catalog(
+        rng=rng,
+        gal_type="exp",
+        coadd_dim=coadd_dim,
+        buff=30,
+        layout="grid",
+    )
+
     _ = make_trivial_sim(
         rng=rng,
-        noise=0.001,
+        galaxy_catalog=galaxy_catalog,
         coadd_dim=351,
-        buff=50,
-        layout='grid',
         g1=0.02,
         g2=0.00,
         dither=dither,
@@ -33,13 +44,20 @@ def test_trivial_sim():
     coadd_dim = 201
     psf_dim = 47
     rng = np.random.RandomState(seed)
+
+    galaxy_catalog = make_galaxy_catalog(
+        rng=rng,
+        gal_type="exp",
+        coadd_dim=coadd_dim,
+        buff=30,
+        layout="grid",
+    )
+
     sim_data = make_trivial_sim(
         rng=rng,
-        noise=0.001,
+        galaxy_catalog=galaxy_catalog,
         coadd_dim=coadd_dim,
-        buff=3,
         psf_dim=psf_dim,
-        layout="grid",
         g1=0.02,
         g2=0.00,
         bands=bands,
@@ -58,22 +76,28 @@ def test_trivial_sim():
 @pytest.mark.parametrize('epochs_per_band', [1, 2, 3])
 def test_trivial_sim_epochs(epochs_per_band):
 
-    bands = ['r', 'i', 'z']
     seed = 7421
+    bands = ["r", "i", "z"]
     coadd_dim = 301
     psf_dim = 47
+
     rng = np.random.RandomState(seed)
-    sim_data = make_trivial_sim(
+
+    galaxy_catalog = make_galaxy_catalog(
         rng=rng,
-        noise=0.001,
+        gal_type="exp",
         coadd_dim=coadd_dim,
         buff=10,
+        layout="grid",
+    )
+
+    sim_data = make_trivial_sim(
+        rng=rng,
+        galaxy_catalog=galaxy_catalog,
+        coadd_dim=coadd_dim,
         psf_dim=psf_dim,
-        layout='grid',
         g1=0.02,
         g2=0.00,
-        dither=True,
-        rotate=True,
         bands=bands,
         epochs_per_band=epochs_per_band,
     )
@@ -86,17 +110,22 @@ def test_trivial_sim_epochs(epochs_per_band):
 
 @pytest.mark.parametrize("layout", ("grid", "random"))
 def test_trivial_sim_layout(layout):
-
-    seed = 812
-    psf_dim = 47
+    seed = 7421
+    coadd_dim = 201
     rng = np.random.RandomState(seed)
+
+    galaxy_catalog = make_galaxy_catalog(
+        rng=rng,
+        gal_type="exp",
+        coadd_dim=coadd_dim,
+        buff=30,
+        layout=layout,
+    )
+
     _ = make_trivial_sim(
         rng=rng,
-        noise=0.001,
-        coadd_dim=351,
-        buff=10,
-        psf_dim=psf_dim,
-        layout=layout,
+        galaxy_catalog=galaxy_catalog,
+        coadd_dim=coadd_dim,
         g1=0.02,
         g2=0.00,
     )
