f81b238859d44ec47288b3510a2c502737ad3cd3
Erin Sheldon <erin.sheldon@gmail.com>
2020-06-15 10:43:46 -0400

    refactor trivial sim
    offsets, catalogs and images generated separately

--- descwl_shear_sims/__init__.py
+++ descwl_shear_sims/__init__.py
@@ -3,4 +3,7 @@ __version__ = '0.3.0'
 
 from .se_obs import SEObs
 from .simple_sim import Sim
-from .trivial_sim import TrivialSim
+from .trivial_sim import (
+    make_trivial_sim,
+    get_trivial_sim_config,
+)
--- descwl_shear_sims/trivial_sim.py
+++ descwl_shear_sims/trivial_sim.py
@@ -9,222 +9,178 @@ WORLD_ORIGIN = galsim.CelestialCoord(
     dec=0 * galsim.degrees,
 )
 
+GRID_N_ON_SIDE = 6
+RANDOM_DENSITY = 80  # per square arcmin
 
-class TrivialSim(object):
+DEFAULT_TRIVIAL_SIM_CONFIG = {
+    'psf_dim': 51,
+    'coadd_dim': 351,
+    'buff': 50,
+    'layout': 'grid',
+    'dither': False,
+    'rotate': False,
+    'bands': ['i'],
+    'epochs_per_band': 1,
+}
+
+
+def make_trivial_sim(
+    *,
+    rng,
+    noise,
+    coadd_dim,
+    buff,
+    layout,
+    g1,
+    g2,
+    psf_dim=51,
+    dither=False,
+    rotate=False,
+    bands=['i'],
+    epochs_per_band=1,
+):
     """
-    simple sim with round galaxies on a grid, with dithers, rotations, possibly
-    multiple epochs
+    Make simulation data
 
     Parameters
     ----------
-    rng: np.RandomState
-        The input random state
+    rng: numpy.random.RandomState
+        Numpy random state
     noise: float
-        Noise level.  If there are multiple epochs, the noise per epoch is set
-        to noise*sqrt(nepochs) to keep s/n constant
+        Noise level for images
+    coadd_dim: int
+        Default 351
+    buff: int
+        Buffer region where no objects will be drawn, default 50
+    layout: string
+        'grid' or 'random'
     g1: float
-        Shear g1
+        Shear g1 for galaxies
     g2: float
-        Shear g2
-    dither: bool
-        If True, dither the images by +/- 0.5 pixels, default False
-    rotate: bool
-        If True, rotate randomly, default False
-    bands: sequence
-        List of band names, default ['i']
-    epochs_per_band: int
-        Number of epochs per band, default 1
-    """
-    def __init__(self,
-                 *, rng, noise, g1, g2,
-                 dither=False, rotate=False,
-                 bands=['i'], epochs_per_band=1):
+        Shear g2 for galaxies
 
-        self.rng = rng
-        self.psf_dim = 51
-        self.coadd_dim = 351
+    """
+    offsets = get_offsets(
+        rng=rng,
+        coadd_dim=coadd_dim,
+        buff=buff,
+        layout=layout,
+    )
+
+    galaxy_catalog = FixedGalaxyCatalog(offsets=offsets, g1=g1, g2=g2)
+    psf = galsim.Gaussian(fwhm=0.8)
+
+    noise_per_epoch = noise*np.sqrt(epochs_per_band)
+
+    se_dim = (
+        int(np.ceil(coadd_dim * np.sqrt(2))) + 20
+    )
+
+    band_data = {}
+    for band in bands:
+        all_obj = galaxy_catalog.get_objects(band=band)
+        seobj_list = []
+        for epoch in range(epochs_per_band):
+            seobs = make_seobs(
+                rng=rng,
+                noise=noise_per_epoch,
+                all_obj=all_obj,
+                dim=se_dim,
+                psf=psf,
+                psf_dim=psf_dim,
+                dither=dither,
+                rotate=rotate,
+            )
+            seobj_list.append(seobs)
+
+        band_data[band] = seobj_list
+
+    coadd_wcs = make_coadd_wcs(coadd_dim)
+
+    return {
+        'band_data': band_data,
+        'coadd_wcs': coadd_wcs,
+        'psf_dims': [psf_dim]*2,
+        'coadd_dims': [coadd_dim]*2,
+    }
+
+
+def make_galaxy_catalog(
+    *,
+    rng,
+    coadd_dim=351,
+    buff=50,
+    layout='grid',
+    g1=0.0,
+    g2=0.0,
+):
+    """
+    Make an object catalog
+
+    Parameters
+    ----------
+    rng: numpy.random.RandomState
+        Numpy random state
+    coadd_dim: int
+        Default 351
+    buff: int
+        Buffer region where no objects will be drawn, default 50
+    layout: string
+        'grid' or 'random'
+    g1: float
+        Shear g1 for galaxies
+    g2: float
+        Shear g2 for galaxies
+    """
+
+    offsets = get_offsets(
+        rng=rng,
+        coadd_dim=coadd_dim,
+        buff=buff,
+        layout=layout,
+    )
+
+    return FixedGalaxyCatalog(offsets=offsets, g1=g1, g2=g2)
+
+
+class FixedGalaxyCatalog(object):
+    """
+    Galaxies of fixed galsim type, flux, and size
+
+    Same for all bands
+    """
+    def __init__(self, *, offsets, g1, g2):
+        self.offsets = offsets
         self.g1 = g1
         self.g2 = g2
-        self.object_data = None
-        noise_per_epoch = noise*np.sqrt(epochs_per_band)
 
-        all_obj = self.get_objects()
+    def get_objects(self, *, band):
+        """
+        get a list of galsim objects
 
-        se_dim = (
-            int(np.ceil(self.coadd_dim * np.sqrt(2))) + 20
-        )
+        Parameters
+        ----------
+        band: string
+            Get objects for this band.  For the fixed
+            catalog, the objects are the same for every band
 
-        self._sim_data = {}
-        for band in bands:
-            se_obslist = []
-            for epoch in range(epochs_per_band):
-                tim = TrivialImage(
-                    rng=rng,
-                    noise=noise_per_epoch,
-                    all_obj=all_obj,
-                    dim=se_dim,
-                    psf_dim=self.psf_dim,
-                    dither=dither,
-                    rotate=rotate,
-                )
-                se_obslist.append(tim.seobs)
-
-            self._sim_data[band] = se_obslist
-
-        self._set_coadd_wcs()
-
-    def get_objects(self):
-
-        n_on_side = 6
-        spacing = self.coadd_dim/(n_on_side+1)*SCALE
-
-        # ix/iy are really on the sky
-        grid = spacing*(np.arange(n_on_side) - (n_on_side-1)/2)
-
+        Returns
+        -------
+        list of galsim objects
+        """
         objlist = []
-        for ix in range(n_on_side):
-            for iy in range(n_on_side):
-                dx = grid[ix] + SCALE*self.rng.uniform(low=-0.5, high=0.5)
-                dy = grid[iy] + SCALE*self.rng.uniform(low=-0.5, high=0.5)
-
-                obj = galsim.Exponential(
-                    half_light_radius=0.5,
-                ).shift(
-                    dx=dx,
-                    dy=dy,
-                )
-                objlist.append(obj)
+        for i in range(self.offsets.size):
+            obj = galsim.Exponential(
+                half_light_radius=0.5,
+            ).shift(
+                dx=self.offsets['dx'][i],
+                dy=self.offsets['dy'][i]
+            )
+            objlist.append(obj)
 
         all_obj = galsim.Add(objlist)
         all_obj = all_obj.shear(g1=self.g1, g2=self.g2)
         return all_obj
-
-    def gen_sim(self):
-        """
-        Returns a dict, keyed by band, with values lists
-        of SEObs.  Currently the band is always 'i' and the
-        lists are length 1
-        """
-        return self._sim_data
-
-    def _set_coadd_wcs(self):
-        # the coadd will be placed on the undithered grid
-        coadd_dims = [self.coadd_dim]*2
-        coadd_cen = (np.array(coadd_dims)-1)/2
-        coadd_origin = galsim.PositionD(x=coadd_cen[1], y=coadd_cen[0])
-        self.coadd_wcs = make_wcs(
-            scale=SCALE,
-            image_origin=coadd_origin,
-            world_origin=WORLD_ORIGIN,
-        )
-
-
-class TrivialImage(object):
-    def __init__(self, *, rng, noise, all_obj, dim,
-                 psf_dim,
-                 dither=False, rotate=False):
-        """
-        make a grid sim with trivial pixel scale, fixed sized
-        exponentials and gaussian psf
-
-        Parameters
-        ----------
-        rng: numpy.RandomState
-            The random number generator
-        noise: float
-            Gaussian noise level
-        g1: float
-            Shear g1
-        g2: float
-            Shear g2
-        dither: bool
-            If set to True, dither randomly by a pixel width
-        rotate: bool
-            If set to True, rotate the image randomly
-        """
-
-        self.psf_dim = psf_dim
-
-        self._psf = galsim.Gaussian(fwhm=0.8)
-
-        dims = [dim]*2
-        cen = (np.array(dims)-1)/2
-
-        se_origin = galsim.PositionD(x=cen[1], y=cen[0])
-        if dither:
-            dither_range = 0.5
-            off = rng.uniform(low=-dither_range, high=dither_range, size=2)
-            self._offset = galsim.PositionD(x=off[0], y=off[1])
-            se_origin = se_origin + self._offset
-        else:
-            self._offset = None
-
-        if rotate:
-            theta = rng.uniform(low=0, high=2*np.pi)
-        else:
-            theta = None
-
-        self.se_wcs = make_wcs(
-            scale=SCALE,
-            theta=theta,
-            image_origin=se_origin,
-            world_origin=WORLD_ORIGIN,
-        )
-
-        # we want to fit them into the coadd region
-        convolved_obj = galsim.Convolve(all_obj, self._psf)
-
-        # everything gets shifted by the dither offset
-        image = convolved_obj.drawImage(
-            nx=dim,
-            ny=dim,
-            wcs=self.se_wcs,
-            offset=self._offset,
-        )
-        weight = image.copy()
-        weight.array[:, :] = 1.0/noise**2
-
-        image.array[:, :] += rng.normal(scale=noise, size=dims)
-        noise_image = image.copy()
-        noise_image.array[:, :] = rng.normal(scale=noise, size=dims)
-
-        bmask = galsim.Image(
-            np.zeros(dims, dtype='i4'),
-            bounds=image.bounds,
-            wcs=image.wcs,
-            dtype=np.int32,
-        )
-
-        self.seobs = SEObs(
-            image=image,
-            noise=noise_image,
-            weight=weight,
-            wcs=self.se_wcs,
-            psf_function=self._psf_func,
-            bmask=bmask,
-        )
-
-    def _psf_func(self, *, x, y, center_psf, get_offset=False):
-        """
-        center_psf is ignored
-        """
-        image_pos = galsim.PositionD(x=x, y=y)
-
-        offset = copy.deepcopy(self._offset)
-
-        if center_psf:
-            print("ignoring request to center psf, using internal offset")
-
-        gsimage = self._psf.drawImage(
-            nx=self.psf_dim,
-            ny=self.psf_dim,
-            offset=offset,
-            wcs=self.se_wcs.local(image_pos=image_pos),
-        )
-        if get_offset:
-            return gsimage, offset
-        else:
-            return gsimage
 
 
 def make_wcs(*, scale, image_origin, world_origin, theta=None):
@@ -250,3 +206,258 @@ def make_wcs(*, scale, image_origin, world_origin, theta=None):
         world_origin=world_origin,
         units=galsim.arcsec,
     )
+
+
+def make_coadd_wcs(coadd_dim):
+    coadd_dims = [coadd_dim]*2
+    coadd_cen = (np.array(coadd_dims)-1)/2
+    coadd_origin = galsim.PositionD(x=coadd_cen[1], y=coadd_cen[0])
+    return make_wcs(
+        scale=SCALE,
+        image_origin=coadd_origin,
+        world_origin=WORLD_ORIGIN,
+    )
+
+
+def get_offsets(
+    *,
+    rng,
+    coadd_dim,
+    buff,
+    layout,
+):
+    """
+    make position offsets for objects
+
+    rng: numpy.random.RandomState
+        Numpy random state
+    coadd_dim: int
+        Dimensions of final coadd
+    buff: int
+        Buffer region where no objects will be drawn
+    layout: string
+        'grid' or 'random'
+    """
+
+    if layout == 'grid':
+        offsets = get_grid_offsets(
+            rng=rng,
+            dim=coadd_dim,
+            n_on_side=GRID_N_ON_SIDE,
+        )
+    elif layout == 'random':
+        # area covered by objects
+        area = ((coadd_dim - 2*buff)*SCALE/60)**2
+        nobj_mean = area * RANDOM_DENSITY
+        nobj = rng.poisson(nobj_mean)
+        offsets = get_random_offsets(
+            rng=rng,
+            dim=coadd_dim,
+            buff=buff,
+            size=nobj,
+        )
+
+    return offsets
+
+
+def get_grid_offsets(*, rng, dim, n_on_side):
+    """
+    get a set of gridded offsets, with random offsets at the pixel scale
+
+    Parameters
+    ----------
+    rng: numpy.random.RandomState
+        The random number generator
+    dim: int
+        Dimensions of the final image
+    n_on_side: int
+        Number of objects on each side
+
+    Returns
+    -------
+    offsets: array
+        Array with dx, dy offset fields for each point, in
+        arcsec
+    """
+    spacing = dim/(n_on_side+1)*SCALE
+
+    ntot = n_on_side**2
+
+    # ix/iy are really on the sky
+    grid = spacing*(np.arange(n_on_side) - (n_on_side-1)/2)
+
+    offsets = np.zeros(ntot, dtype=[('dx', 'f8'), ('dy', 'f8')])
+
+    i = 0
+    for ix in range(n_on_side):
+        for iy in range(n_on_side):
+            dx = grid[ix] + SCALE*rng.uniform(low=-0.5, high=0.5)
+            dy = grid[iy] + SCALE*rng.uniform(low=-0.5, high=0.5)
+
+            offsets['dx'][i] = dx
+            offsets['dy'][i] = dy
+            i += 1
+
+    return offsets
+
+
+def get_random_offsets(*, rng, dim, buff, size):
+    """
+    get a set of gridded offsets, with random offsets at the pixel scale
+
+    Parameters
+    ----------
+    rng: numpy.random.RandomState
+        The random number generator
+    dim: int
+        Dimensions of the final image
+    n_on_side: int
+        Number of objects on each side
+
+    Returns
+    -------
+    offsets: array
+        Array with dx, dy offset fields for each point, in
+        arcsec
+    """
+
+    halfwidth = (dim - 2*buff)/2.0
+
+    low = -halfwidth*SCALE
+    high = halfwidth*SCALE
+
+    offsets = np.zeros(size, dtype=[('dx', 'f8'), ('dy', 'f8')])
+
+    offsets['dx'] = rng.uniform(low=low, high=high, size=size)
+    offsets['dy'] = rng.uniform(low=low, high=high, size=size)
+
+    return offsets
+
+
+class FixedPSF(object):
+    def __init__(self, *, psf, offset, psf_dim, wcs):
+        self._psf = psf
+        self._offset = offset
+        self._psf_dim = psf_dim
+        self._wcs = wcs
+
+    def __call__(self, *, x, y, center_psf, get_offset=False):
+        """
+        center_psf is ignored
+        """
+        image_pos = galsim.PositionD(x=x, y=y)
+
+        offset = copy.deepcopy(self._offset)
+
+        if center_psf:
+            print("ignoring request to center psf, using internal offset")
+
+        gsimage = self._psf.drawImage(
+            nx=self._psf_dim,
+            ny=self._psf_dim,
+            offset=offset,
+            wcs=self._wcs.local(image_pos=image_pos),
+        )
+        if get_offset:
+            return gsimage, offset
+        else:
+            return gsimage
+
+
+def make_seobs(
+    *,
+    rng,
+    noise,
+    all_obj,
+    dim,
+    psf,
+    psf_dim,
+    dither=False,
+    rotate=False,
+):
+    """
+    make a grid sim with trivial pixel scale, fixed sized
+    exponentials and gaussian psf
+
+    Parameters
+    ----------
+    rng: numpy.random.RandomState
+        The random number generator
+    noise: float
+        Gaussian noise level
+    g1: float
+        Shear g1
+    g2: float
+        Shear g2
+    dither: bool
+        If set to True, dither randomly by a pixel width
+    rotate: bool
+        If set to True, rotate the image randomly
+    """
+
+    dims = [dim]*2
+    cen = (np.array(dims)-1)/2
+
+    se_origin = galsim.PositionD(x=cen[1], y=cen[0])
+    if dither:
+        dither_range = 0.5
+        off = rng.uniform(low=-dither_range, high=dither_range, size=2)
+        offset = galsim.PositionD(x=off[0], y=off[1])
+        se_origin = se_origin + offset
+    else:
+        offset = None
+
+    if rotate:
+        theta = rng.uniform(low=0, high=2*np.pi)
+    else:
+        theta = None
+
+    se_wcs = make_wcs(
+        scale=SCALE,
+        theta=theta,
+        image_origin=se_origin,
+        world_origin=WORLD_ORIGIN,
+    )
+
+    # we want to fit them into the coadd region
+    convolved_objects = galsim.Convolve(all_obj, psf)
+
+    # everything gets shifted by the dither offset
+    image = convolved_objects.drawImage(
+        nx=dim,
+        ny=dim,
+        wcs=se_wcs,
+        offset=offset,
+    )
+    weight = image.copy()
+    weight.array[:, :] = 1.0/noise**2
+
+    image.array[:, :] += rng.normal(scale=noise, size=dims)
+    noise_image = image.copy()
+    noise_image.array[:, :] = rng.normal(scale=noise, size=dims)
+
+    bmask = galsim.Image(
+        np.zeros(dims, dtype='i4'),
+        bounds=image.bounds,
+        wcs=image.wcs,
+        dtype=np.int32,
+    )
+
+    psf_obj = FixedPSF(psf=psf, offset=offset, psf_dim=psf_dim, wcs=se_wcs)
+
+    return SEObs(
+        image=image,
+        noise=noise_image,
+        weight=weight,
+        wcs=se_wcs,
+        psf_function=psf_obj,
+        bmask=bmask,
+    )
+
+
+def get_trivial_sim_config(config=None):
+    out_config = copy.deepcopy(DEFAULT_TRIVIAL_SIM_CONFIG)
+
+    if config is not None:
+        out_config.update(config)
+    return out_config
