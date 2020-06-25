3274ab2361cda30231f2160b97a4c760489890da
Erin Sheldon <erin.sheldon@gmail.com>
2020-06-25 14:41:56 -0400

    added test that image and noise covariances are consistent
    won't run in travis since it is doing
    
    coadd_mod = pytest.importorskip('descwl_coadd.coadd')

---
+++ descwl_shear_sims/tests/test_correlated_noise.py
@@ -0,0 +1,94 @@
+import pytest
+import numpy as np
+from descwl_shear_sims.trivial_sim import (
+    make_trivial_sim,
+    FixedGalaxyCatalog,
+    make_psf,
+)
+from numba import njit
+
+
+@njit
+def get_cov(image):
+
+    cov = np.zeros((3, 3))
+
+    nrow, ncol = image.shape
+    for row in range(1, nrow-1):
+        for col in range(1, ncol-1):
+
+            cenval = image[row, col]
+            for row_offset in range(-1, 1+1):
+                for col_offset in range(-1, 1+1):
+                    val = image[row+row_offset, col+col_offset]
+
+                    cov[row_offset+1, col_offset+1] += cenval*val
+    npix = (nrow - 2)*(ncol - 2)
+    cov *= 1.0/npix
+    return cov
+
+
+def test_correlated_noise():
+    """
+    use mag 37 galaxies so the image is purely noise,
+    and compare statistics with the coadded noise image
+    """
+    coadd_mod = pytest.importorskip('descwl_coadd.coadd')
+
+    rng0 = np.random.RandomState(97756)
+    ntrial = 100
+    for i in range(ntrial):
+        seed = rng0.randint(0, 2**30)
+        rng = np.random.RandomState(seed)
+
+        coadd_dim = 301
+        galaxy_catalog = FixedGalaxyCatalog(
+            rng=rng,
+            coadd_dim=coadd_dim,
+            buff=0,
+            layout="grid",
+            mag=37.0,
+            hlr=0.5,
+        )
+
+        psf = make_psf(psf_type="gauss")
+        sim_data = make_trivial_sim(
+            rng=rng,
+            galaxy_catalog=galaxy_catalog,
+            coadd_dim=coadd_dim,
+            g1=0.02,
+            g2=0.00,
+            psf=psf,
+            dither=True,
+            rotate=True,
+        )
+
+        mbc = coadd_mod.MultiBandCoadds(
+            rng=rng,
+            data=sim_data['band_data'],
+            coadd_wcs=sim_data['coadd_wcs'],
+            coadd_dims=sim_data['coadd_dims'],
+            psf_dims=sim_data['psf_dims'],
+            byband=False,
+            loglevel="debug",
+        )
+
+        coadd_obs = mbc.coadds['all']
+        image = coadd_obs.image
+        noise = coadd_obs.noise
+
+        icov = get_cov(image)
+        ncov = get_cov(noise)
+
+        if i == 0:
+            icov_sum = icov
+            ncov_sum = ncov
+        else:
+            icov_sum += icov
+            ncov_sum += ncov
+
+    icov = icov_sum/ntrial
+    ncov = ncov_sum/ntrial
+    fracdiff = ncov/icov - 1
+
+    assert np.all(np.abs(fracdiff) < 0.01)
