f9e7ccbdc29b1b349d7ab299a2a1ba849344e9cd
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-12-01 06:35:41 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="descwl_shear_sims",
     version="0.3.0",
     packages=find_packages(),
-    install_requires=['numpy', 'galsim'],
     include_package_data=True,
     author='Matthew R. Becker',
     author_email='becker.mr@gmail.com',
