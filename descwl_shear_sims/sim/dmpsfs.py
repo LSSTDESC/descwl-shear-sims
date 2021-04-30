41237326678a95cd106ad0acf6565b035b3cd758
Erin Sheldon <erin.sheldon@gmail.com>
2021-04-30 13:35:19 -0400

    bug using // rather than %

--- descwl_shear_sims/sim/dmpsfs.py
+++ descwl_shear_sims/sim/dmpsfs.py
@@ -50,7 +50,7 @@ class FixedDMPSF(ImagePsf):
         """
         ImagePsf.__init__(self)
 
-        if psf_dim // 2 == 0:
+        if psf_dim % 2 == 0:
             raise ValueError('psf dims must be odd, got %s' % psf_dim)
 
         self._psf_dim = psf_dim
