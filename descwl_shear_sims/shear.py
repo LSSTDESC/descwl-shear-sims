da227e6de899dad2b4efc2b8a8a1c55aca38ab6d
Xiangchong Li <mr.superonion@hotmail.com>
2023-07-25 03:06:21 -0700

    change to use galsim.PositionD object and Shear object; set the kappa_max=0.8

--- descwl_shear_sims/shear.py
+++ descwl_shear_sims/shear.py
@@ -1,33 +1,34 @@
+import galsim
 import numpy as np
 # maximum kappa allowed
 # values greater than it will be clipped
-kappa_max = 0.6
-
-# factor from arcsec to radians
-arcsec2rad = 1./3600./180.*np.pi
-
+kappa_max = 0.8
 
 def get_shear(
     *,
     shear_type="constant",
     z_gals=None,
-    shifts=None,
+    shift=None,
     **kwargs,
-    ):
+):
     """
     A shear wrapper to return g1 and g2 with different shear type
 
     Parameters
     ----------
-    cluster_obj:    cluster object
+    shear_type:     the constant shear or shear from NFW halos
+    z_gals:         redshifts of galaxies
+    shift:          Galsim positionD shift
     """
     if shear_type == "constant":
         shear_obj = ShearConstant(**kwargs)
+    elif shear_type =="redshift":
+        shear_obj = ShearRedshift(**kwargs)
     elif shear_type == "NFW":
         shear_obj = ShearNFW(**kwargs)
     else:
         raise ValueError("Do not support the shear type: %s" % shear_type)
-    return shear_obj.get_shear(z_gals, shifts)
+    return shear_obj.get_shear(z_gals, shift)
 
 
 class ShearNFW(object):
@@ -50,28 +51,59 @@ class ShearNFW(object):
         self.cosmo = cluster_obj.cosmo
         return
 
+    def get_shear(self, z_gals, shift):
+        """
+        A shear wrapper to return g1 and g2 with different shear type
 
-    def get_shear(self, z_gals, shifts):
+        Parameters
+        ----------
+        z_gals (float):             redshifts of galaxies
+        shift (galsim.positionD):  Galsim positionD shift [arcsec]
+
+        Returns
+        ---------
+        shear (galsim.Shear)        shear distortion on the galaxy
+        """
         from astropy.coordinates import SkyCoord
         z_cl = self.z_cl
         # Create the SkyCoord objects
         coord_cl = SkyCoord(self.ra_cl, self.dec_cl, unit="arcsec")
-        coord_gals = SkyCoord(shifts["dx"], shifts["dy"], unit="arcsec")
+        coord_gals = SkyCoord(shift.x, shift.y, unit="arcsec")
         # Calculate the separation
         sep = coord_cl.separation(coord_gals).radian
+        # What is the unit of r3d?
         r3d = self.cosmo.rad2mpc(sep, self.z_cl)
+        # position angle
         phi = coord_cl.position_angle(coord_gals).radian
 
         # TODO: confirm whether the units is Mpc/h or Mpc?
-        DeltaSigma = self.cobj.eval_excess_surface_density(r3d, z_cl)
         gammat = self.cobj.eval_tangential_shear(r3d, z_cl, z_gals)
-        kappa = self.cobj.eval_convergence(r3d, z_cl, z_gals)
-        gamma1 = gammat * np.cos(2. * phi)
-        gamma2 = -gammat * np.sin(2. * phi)
-        return gamma1/(1-kappa), gamma2/(1-kappa)
+        kappa0 = self.cobj.eval_convergence(r3d, z_cl, z_gals)
+        # we are forcing kappa to be less than kappa_max
+        # and scale gamma by the same ratio
+        kappa = min(kappa0, kappa_max)
+        ratio = kappa / kappa0
+        gamma1 = gammat * np.cos(2. * phi) * ratio
+        gamma2 = -gammat * np.sin(2. * phi) * ratio
+        shear = galsim.Shear(g1=gamma1/(1-kappa), g2=gamma2/(1-kappa))
+        return shear
 
 
 class ShearConstant(object):
+    """
+    Constant shear along every redshift slice
+    """
+    def __init__(self, g1, g2):
+        self.g1 = g1
+        self.g2 = g2
+        return
+
+    def get_shear(self):
+        shear = galsim.Shear(g1= self.g1, g2=self.g2)
+        return shear
+
+
+class ShearRedshift(object):
     """
     Constant shear along every redshift slice
     """
@@ -90,10 +122,11 @@ class ShearConstant(object):
         self.g_dist = g_dist
         return
 
-    def get_shear(self, z_gals=None, shifts=None):
+    def get_shear(self, z_gals=None, shift=None):
         if z_gals is None:
             assert self.mode == "0" * self.nz_bins
-        z_gal_bins = z_gals // self.dz_bin
+        #z_gal_bins = z_gals // self.dz_bin
         gamma1, gamma2 = (None, None)
         # TODO: Finish implementing the z-dependent shear
-        return gamma1, gamma2
+        shear = galsim.Shear(g1= gamma1, g2=gamma2)
+        return shear
