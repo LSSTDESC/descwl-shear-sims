58bb7881bed865e5c26d5632bd1dfee384cb5192
Zhou Conghao <zchusre@gmail.com>
2024-09-07 10:59:57 -0700

    add test to ensure the sky origin for se and coadd are the same when using simple coadd wcs

--- descwl_shear_sims/tests/test_dmwcs.py
+++ descwl_shear_sims/tests/test_dmwcs.py
@@ -2,8 +2,9 @@ import numpy as np
 import galsim
 import lsst.afw.image as afw_image
 import lsst.geom as geom
-from ..wcs import make_dm_wcs, make_coadd_dm_wcs_simple
+from ..wcs import make_wcs, make_dm_wcs, make_coadd_dm_wcs_simple
 from ._wcs import make_sim_wcs, SCALE
+from ..sim import get_coadd_center_gs_pos
 
 
 def test_dmwcs():
@@ -47,10 +48,12 @@ def test_dmwcs():
     # stop
 
     assert np.allclose(
-        skypos.getRa().asRadians(), gs_skypos.ra / galsim.radians,
+        skypos.getRa().asRadians(),
+        gs_skypos.ra / galsim.radians,
     )
     assert np.allclose(
-        skypos.getDec().asRadians(), gs_skypos.dec / galsim.radians,
+        skypos.getDec().asRadians(),
+        gs_skypos.dec / galsim.radians,
     )
 
     impos = dm_wcs.skyToPixel(skypos)
@@ -97,12 +100,64 @@ def test_coadd_dmwcs_simple():
     print("galsim sky position ra dec", gs_skypos.ra, gs_skypos.dec)
 
     assert np.allclose(
-        skypos.getRa().asRadians(), gs_skypos.ra / galsim.radians,
+        skypos.getRa().asRadians(),
+        gs_skypos.ra / galsim.radians,
     )
     assert np.allclose(
-        skypos.getDec().asRadians(), gs_skypos.dec / galsim.radians,
+        skypos.getDec().asRadians(),
+        gs_skypos.dec / galsim.radians,
     )
 
     impos = dm_coadd_wcs_simple.skyToPixel(skypos)
     assert np.allclose(impos.x, x - 1)
     assert np.allclose(impos.y, y - 1)
+
+
+def test_same_world_origin_se_coadd_wcs_simple():
+    se_dim = 30
+    coadd_dim = 20
+
+    dims = [se_dim] * 2
+    # Galsim uses 1 offset. An array with length =dim=5
+    # The center is at 3=(5+1)/2
+    cen = (np.array(dims) + 1) / 2
+    se_origin = galsim.PositionD(x=cen[1], y=cen[0])
+
+
+    masked_image = afw_image.MaskedImageF(coadd_dim, coadd_dim)
+    exp = afw_image.ExposureF(masked_image)
+
+    tcoadd_dm_wcs_simple, coadd_bbox = make_coadd_dm_wcs_simple(coadd_dim, SCALE)
+
+    exp.setWcs(tcoadd_dm_wcs_simple)
+
+    dm_coadd_wcs_simple = exp.getWcs()
+
+    coadd_bbox_cen_gs_skypos = get_coadd_center_gs_pos(
+        coadd_wcs=dm_coadd_wcs_simple,
+        coadd_bbox=coadd_bbox,
+    )
+
+    se_wcs = make_wcs(
+        scale=SCALE,
+        theta=0,
+        image_origin=se_origin,
+        world_origin=coadd_bbox_cen_gs_skypos,
+    )
+    dm_se_wcs = make_dm_wcs(se_wcs)
+
+    se_sky_origin = dm_se_wcs.getSkyOrigin()
+    coadd_sky_origin = dm_coadd_wcs_simple.getSkyOrigin()
+
+    print("se sky origin", se_sky_origin)
+    print("coadd sky origin", coadd_sky_origin)
+
+    assert np.allclose(
+        se_sky_origin.getRa().asRadians(),
+        coadd_sky_origin.getRa().asRadians(),
+    )
+
+    assert np.allclose(
+        se_sky_origin.getDec().asRadians(),
+        coadd_sky_origin.getDec().asRadians(),
+    )
