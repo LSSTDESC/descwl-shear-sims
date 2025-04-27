45db9e56e2c9fcee4022eeb57fbcd4140151d0b2
Zhou Conghao <zchusre@gmail.com>
2025-04-27 13:39:44 -0700

    update test to make unordered comparison

--- descwl_shear_sims/tests/test_sim.py
+++ descwl_shear_sims/tests/test_sim.py
@@ -690,14 +690,14 @@ def test_sim_truth_info():
         survey_name=survey_name,
     )
     assert "truth_info" in out.keys()
-    assert out["truth_info"].dtype.names == (
+    assert set(out["truth_info"].dtype.names) == {
         'index', 'ra', 'dec', 'z', 'image_x', 'image_y',
         'prelensed_image_x', 'prelensed_image_y',
         'prelensed_ra', 'prelensed_dec',
         'shift_x', 'shift_y',
         'lensed_shift_x', 'lensed_shift_y',
         'kappa', 'gamma1', 'gamma2'
-    )
+    }
     np.testing.assert_allclose(
         galaxy_catalog.indices,
         out["truth_info"]["index"],
